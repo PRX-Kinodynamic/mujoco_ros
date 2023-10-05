28fcaa35e52e98c38687efec91d8dd02cbdd7a0a
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-10-05 13:17:55 -0400

    Adding callbacks for mouse movement and click

--- mujoco/include/mujoco/simulator.hpp
+++ mujoco/include/mujoco/simulator.hpp
@@ -16,9 +16,6 @@ class mujoco_simulator_t : public std::enable_shared_from_this<mujoco_simulator_
 
   GLFWwindow* window;
 
-  protected:
-  mjrRect viewport;
-
   public:
   mjModel* m;
   mjData* d;
@@ -48,6 +45,8 @@ class mujoco_simulator_t : public std::enable_shared_from_this<mujoco_simulator_
     visualize = _visualize;
     if (visualize)
     {
+      button_left = button_middle = button_right = false;
+      lastx = lasty = 0;
       if (!glfwInit())
         std::cerr << "Error in initializing GLFW." << std::endl;
 
@@ -64,6 +63,20 @@ class mujoco_simulator_t : public std::enable_shared_from_this<mujoco_simulator_
       mjv_makeScene(m, &scn, 1000);
       mjr_makeContext(m, &con, mjFONTSCALE_150);
       viewport = { 0, 0, 1200, 900 };
+
+      glfwSetWindowUserPointer(window, this);
+      glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+        auto sim = static_cast<mujoco_simulator_t*>(glfwGetWindowUserPointer(window));
+        sim->mouse_move(window, xpos, ypos);
+      });
+      glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+        auto sim = static_cast<mujoco_simulator_t*>(glfwGetWindowUserPointer(window));
+        sim->mouse_button(window, button, action, mods);
+      });
+      glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+        auto sim = static_cast<mujoco_simulator_t*>(glfwGetWindowUserPointer(window));
+        sim->scroll(window, xoffset, yoffset);
+      });
     }
   }
 
@@ -157,5 +170,61 @@ class mujoco_simulator_t : public std::enable_shared_from_this<mujoco_simulator_
       trajectory.clear();
       add_current_state_to_trajectory();
     }
+  }
+
+  protected:
+  mjrRect viewport;
+  bool button_left, button_right, button_middle;
+  double lastx, lasty;
+
+  void mouse_button(GLFWwindow* window, int button, int act, int mods)
+  {
+    button_left =   (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
+    button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
+    button_right =  (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
+    glfwGetCursorPos(window, &lastx, &lasty);
+  }
+  void mouse_move(GLFWwindow* window, double xpos, double ypos)
+  {
+    if (!button_left && !button_middle && !button_right)
+    {
+      return;
+    }
+
+    // compute mouse displacement, save
+    double dx = xpos - lastx;
+    double dy = ypos - lasty;
+    lastx = xpos;
+    lasty = ypos;
+
+    // get current window size
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    // get shift key state
+    bool mod_shift =
+        (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS);
+
+    // determine action based on mouse button
+    mjtMouse action;
+    if (button_right)
+    {
+      action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
+    }
+    else if (button_left)
+    {
+      action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
+    }
+    else
+    {
+      action = mjMOUSE_ZOOM;
+    }
+
+    // move camera
+    mjv_moveCamera(m, action, dx / height, dy / height, &scn, &cam);
+  }
+  void scroll(GLFWwindow* window, double xoffset, double yoffset)
+  {
+    mjv_moveCamera(m, mjMOUSE_ZOOM, 0, 0.05 * yoffset, &scn, &cam);
   }
 };
