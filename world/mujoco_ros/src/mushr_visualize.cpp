921d525d982aca2f7f13b986c673e44eb2dfe876
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-11-07 11:24:06 -0500

    Revert "Adding a ground-truth pose publisher for simulation"
    This reverts commit da84f79350e1888137e6dd88b6344e5648d9f0a3.

--- world/mujoco_ros/src/mushr_visualize.cpp
+++ world/mujoco_ros/src/mushr_visualize.cpp
@@ -1,6 +1,6 @@
 #include <thread>
 #include "mujoco_ros/control_listener.hpp"
-#include "mujoco_ros/ground_truth_pose.hpp"
+#include "mujoco_ros/control_listener.hpp"
 #include "prx_models/mj_mushr.hpp"
 
 // Mujoco-Ros visualization in (almost) RT:
@@ -9,8 +9,7 @@ int main(int argc, char** argv)
 {
   using CtrlMsg = prx_models::MushrControl;
   using PlanMsg = prx_models::MushrPlan;
-  const std::string root{ ros::this_node::getNamespace() };
-  const std::string node_name{ root + "/Simulation" };
+  const std::string node_name{ "Mujoco_Ros_viz_example" };
   ros::init(argc, argv, node_name);
   ros::NodeHandle n;
 
@@ -35,27 +34,24 @@ int main(int argc, char** argv)
     ROS_ERROR("Failed to get param 'save_trajectory'.");
   }
 
+  const std::string root{ ros::this_node::getNamespace() };
   mj_ros::SimulatorPtr sim{ mj_ros::simulator_t::initialize(model_path, save_trajectory) };
   controller_listener_t<CtrlMsg, PlanMsg> controller_listener(n, sim->d);
-  mj_ros::ground_truth_pose_t<prx_models::MushrObservation> ground_truth_pose(n, sim, 30);
 
   ros::Subscriber reset_subscriber;
   reset_subscriber = n.subscribe(root + "/reset", 1000, &mj_ros::simulator_t::reset_simulation, sim.get());
 
   // Set the threads
   std::thread step_thread(&mj_ros::simulator_t::run, &(*sim));  // Mj sim
-  std::thread publisher_thread(&mj_ros::ground_truth_pose_t<prx_models::MushrObservation>::run, &ground_truth_pose);
   mj_ros::simulator_visualizer_t visualizer{ sim };             // Mj Viz
   ros::AsyncSpinner spinner(1);                                 // 1 thread for the controller
 
-  
   // Run threads: Mj sim is already running at this point
   spinner.start();
   visualizer();  // Blocking
 
   // Join the non-visual threads
   step_thread.join();
-  publisher_thread.join();
   spinner.stop();
 
   ROS_INFO_STREAM(node_name << " finished.");
