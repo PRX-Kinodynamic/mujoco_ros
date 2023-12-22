d19a6f524db8236bcd8dee81253ce7123cd3c5ba
Edgar <gary.granados@gmail.com>
2023-12-22 17:39:04 -0500

    * Adding service to change plan file  * Tested on musher

--- infrastructure/ml4kp_bridge/src/nodelets/plan_from_file_publisher.cpp
+++ infrastructure/ml4kp_bridge/src/nodelets/plan_from_file_publisher.cpp
@@ -1,8 +1,11 @@
+#include <filesystem>
+
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.hpp>
 
 #include <ml4kp_bridge/plan_bridge.hpp>
+#include <ml4kp_bridge/GetStringSrv.h>
 #include <std_srvs/Empty.h>
 namespace ml4kp_bridge
 {
@@ -34,13 +37,34 @@ protected:
     copy(_plan_msg, _plan);
 
     // const std::string publisher_name{ ros::this_node::getNamespace() + "/plan" };
-    const std::string service_name{ ros::this_node::getNamespace() + "/publish_plan" };
+    const std::string service_name{ ros::this_node::getNamespace() + "/plan_reader/publish_plan" };
+    const std::string service_plan_filename{ ros::this_node::getNamespace() + "/plan_reader/set_plan" };
 
     _timer = private_nh.createTimer(ros::Duration(1.0), &plan_from_file_t::timer_callback, this, true, false);
     _publisher = private_nh.advertise<ml4kp_bridge::PlanStamped>(_publisher_topic, 1, true);
 
     _service = private_nh.advertiseService(service_name, &plan_from_file_t::service_callback, this);
+    _service_filename = private_nh.advertiseService(service_plan_filename, &plan_from_file_t::filename_callback, this);
   }
+
+  bool filename_callback(ml4kp_bridge::GetStringSrv::Request& request, ml4kp_bridge::GetStringSrv::Response& response)
+  {
+    const std::filesystem::path filename{ request.string };
+
+    if (std::filesystem::exists(filename))
+    {
+      _plan->clear();
+      _plan->from_file(request.string);
+      copy(_plan_msg, _plan);
+      response.ok = true;
+    }
+    else
+    {
+      response.ok = false;
+    }
+    return response.ok;
+  }
+
   bool service_callback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
   {
     _timer.start();
@@ -68,6 +92,7 @@ protected:
   ros::Timer _timer;
   ros::Publisher _publisher;
   ros::ServiceServer _service;
+  ros::ServiceServer _service_filename;
 };
 }  // namespace ml4kp_bridge
 PLUGINLIB_EXPORT_CLASS(ml4kp_bridge::plan_from_file_t, nodelet::Nodelet);
