26420c92c4a408c6100f39dffbea5852868f90e0
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2024-01-03 18:18:29 -0500

    Removing old interface and replace with nodelets

--- infrastructure/interface/src/nodelets/translators.cpp
+++ infrastructure/interface/src/nodelets/translators.cpp
@@ -4,8 +4,6 @@
 #include <pluginlib/class_list_macros.hpp>
 
 #include <ml4kp_bridge/defs.h>
-#include <prx_models/mj_copy.hpp>
-#include <prx_models/mj_mushr.hpp>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 
 #include "interface/ackermann_msg.hpp"
@@ -17,6 +15,9 @@ namespace interface
 using MushrFromSpacePointNodelet = interface::msg_translator_t<prx_models::MushrControl, ml4kp_bridge::SpacePoint>;
 using AckermannFromSpacePointNodelet =
     interface::msg_translator_t<ackermann_msgs::AckermannDriveStamped, ml4kp_bridge::SpacePoint>;
+using GroundTruthPoseNodelet = 
+    interface::msg_translator_t<prx_models::MushrObservation, mujoco_ros::SensorDataStamped>;
 }  // namespace interface
 PLUGINLIB_EXPORT_CLASS(interface::MushrFromSpacePointNodelet, nodelet::Nodelet);
 PLUGINLIB_EXPORT_CLASS(interface::AckermannFromSpacePointNodelet, nodelet::Nodelet);
+PLUGINLIB_EXPORT_CLASS(interface::GroundTruthPoseNodelet, nodelet::Nodelet);
