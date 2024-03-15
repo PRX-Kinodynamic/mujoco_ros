f48f9c3bbf721e9cb397ede9d22a0ec166089798
Edgar <gary.granados@gmail.com>
2024-03-15 14:16:20 -0400

    Adding reset topic + functionality to execution status

--- infrastructure/utils/include/utils/execution_status.hpp
+++ infrastructure/utils/include/utils/execution_status.hpp
@@ -12,13 +12,19 @@ public:
   enum Status
   {
     running,
+    reset,
     stop_execution,
   };
 
   execution_status_t(ros::NodeHandle& nh, const std::string topic_name)
     : _status(Status::running), _topic_name(topic_name)
   {
-    _subscriber = nh.subscribe(topic_name + "/stop", 1, &execution_status_t::callback, this);
+    _subscriber = nh.subscribe<std_msgs::Empty>(
+        topic_name + "/stop", 1,
+        boost::bind(&execution_status_t::callback, this, _1, utils::execution_status_t::Status::stop_execution));
+    _subscriber_reset = nh.subscribe<std_msgs::Empty>(
+        topic_name + "/reset", 1,
+        boost::bind(&execution_status_t::callback, this, _1, utils::execution_status_t::Status::reset));
   }
 
   inline Status status() const
@@ -31,21 +37,25 @@ public:
     _status = status;
   }
 
+  // Return true if a reset signal has been received. The status is changed to running
+  inline bool reset_received()
+  {
+    const bool status{ _status == utils::execution_status_t::Status::reset };
+    _status = utils::execution_status_t::Status::running;
+    return status;
+  }
   // Convinient method to do: `while(stop_execution.continue_running()){(...)}
   inline bool ok() const
   {
-    return ros::ok() && _status == utils::execution_status_t::Status::running;
+    return ros::ok() && _status != utils::execution_status_t::Status::stop_execution;
   }
 
 private:
-  void callback(const std_msgs::Empty& msg)
+  void callback(const std_msgs::EmptyConstPtr& msg, const utils::execution_status_t::Status status)
   {
-    // res.success = true;
-    // res.message = _service_name;
-    _status = utils::execution_status_t::Status::stop_execution;
-    // return true;
+    _status = status;
   }
-  ros::Subscriber _subscriber;
+  ros::Subscriber _subscriber, _subscriber_reset;
   Status _status;
   const std::string _topic_name;
 };
