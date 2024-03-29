180839fe7914a3484ad1bc8d40aaa633b89621cd
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2024-03-29 14:18:04 -0400

    Adding control propagation service

--- control/src/nodelets/mushr_feedback_controller.cpp
+++ control/src/nodelets/mushr_feedback_controller.cpp
@@ -5,6 +5,7 @@
 #include <pluginlib/class_list_macros.h>
 
 #include "prx_models/mj_mushr.hpp"
+#include <control/MushrControlPropagation.h>
 #include <ml4kp_bridge/TrajectoryStamped.h>
 #include <prx_models/MushrObservation.h>
 #include <std_msgs/Empty.h>
@@ -22,15 +23,30 @@ public:
     auto ml4kp_params = prx::param_loader(params_file);
     auto controller_params = prx::param_loader(ml4kp_params["feedback_controller"].as<std::string>());
 
+    prx::simulation_step = ml4kp_params["simulation_step"].as<double>();
     std::string plant_name = ml4kp_params["/plant/name"].as<>();
     std::string plant_path = ml4kp_params["/plant/path"].as<>();
     plant = prx::system_factory_t::create_system(plant_name, plant_path);
     ROS_ASSERT(plant != nullptr);
 
+    std::vector<double> min_state_limits = ml4kp_params["/plant/state_space/lower_bound"].as<std::vector<double>>();
+    std::vector<double> max_state_limits = ml4kp_params["/plant/state_space/upper_bound"].as<std::vector<double>>();
+    plant->get_state_space()->set_bounds(min_state_limits, max_state_limits);
+    std::vector<double> param_values = ml4kp_params["/plant/parameter_space/values"].as<std::vector<double>>();
+    plant->get_parameter_space()->copy_from(param_values);
+
     // Control space limits of the real MuSHR
-    std::vector<double> min_control_limits = {-1., -1.};
-    std::vector<double> max_control_limits = {1., 1.};
+    std::vector<double> min_control_limits = { -1., -1. };
+    std::vector<double> max_control_limits = { 1., 1. };
     plant->get_control_space()->set_bounds(min_control_limits, max_control_limits);
+
+    // prx::world_model_t control_model({ plant }, {});
+    std::vector<prx::system_ptr_t> systems = { plant };
+    std::vector<std::shared_ptr<prx::movable_object_t>> obstacles = {};
+    control_model = std::make_shared<prx::world_model_t>(systems, obstacles);
+    control_model->create_context("control_context", { plant_name }, {});
+    context = control_model->get_context("control_context");
+    ROS_ASSERT(context.first != nullptr);
 
     trajectory = std::make_shared<prx::trajectory_t>(plant->get_state_space());
     current_state = plant->get_state_space()->make_point();
@@ -70,6 +86,9 @@ public:
 
     _control_publisher = nh.advertise<ml4kp_bridge::SpacePoint>(control_topic, 1, true);
     _ctrl.point.resize(plant->get_control_space()->get_dimension());
+
+    _service_server =
+        nh.advertiseService("/mushr/control_propagation", &mushr_feedback_controller_t::control_propagation, this);
   }
 
   void get_trajectory(const ml4kp_bridge::TrajectoryStamped& msg)
@@ -100,10 +119,35 @@ public:
     _control_publisher.publish(_ctrl);
   }
 
+  bool control_propagation(control::MushrControlPropagation::Request& req,
+                           control::MushrControlPropagation::Response& res)
+  {
+    if (!_trajectory_received || !_pose_received)
+    {
+      return false;
+    }
+    prx::space_point_t propagation_current_state = plant->get_state_space()->clone_point(current_state);
+    prx::plan_t plan(plant->get_control_space());
+    plan.append_onto_back(controller->control_duration());
+    double total_duration = req.duration.data.toSec();
+    unsigned num_steps = total_duration / plan.back().duration;
+    Eigen::VectorXd propagation_control;
+    for (unsigned i = 0; i < num_steps; i++)
+    {
+      controller->get_control(propagation_current_state, propagation_control);
+      plant->get_control_space()->copy(plan.back().control, propagation_control);
+      plant->get_control_space()->enforce_bounds(plan.back().control);
+      context.first->propagate(propagation_current_state, plan, propagation_current_state);
+    }
+    ml4kp_bridge::copy(res.final_state, propagation_current_state);
+    return true;
+  }
+
 private:
   ros::Subscriber _trajectory_subscriber, _pose_subscriber, _reset_subscriber;
   ros::Publisher _control_publisher;
   ros::Timer _timer;
+  ros::ServiceServer _service_server;
 
   prx_models::MushrObservation _pose;
   ml4kp_bridge::SpacePoint _ctrl;
@@ -111,6 +155,8 @@ private:
 
   prx::system_ptr_t plant;
   prx::feedback_controller_t<prx::trajectory_t>* controller;
+  std::shared_ptr<prx::world_model_t> control_model;
+  prx::world_model_context context;
   std::shared_ptr<prx::trajectory_t> trajectory;
   prx::space_point_t current_state;
   Eigen::VectorXd current_control;
